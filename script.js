4790875b7e0a341327bbce6f92c00ca8579801c9
Saajnaa <saajnaa.123@gmail.com>
2025-01-06 09:35:08 +0545

    header improving..

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 // ===== responsive navbar button code ======//
 
 const mobile_navbar_btn = document.querySelector(".mobile-navbar-btn");
+
 const nav = document.querySelector("nav");
 nav.classList.remove("active")
+
+
 const toggle_nav_bar = () => {
+
   nav.classList.toggle("active");
+
 };
+
 mobile_navbar_btn.addEventListener("click", () => toggle_nav_bar());
 
-
-
-
+ 
